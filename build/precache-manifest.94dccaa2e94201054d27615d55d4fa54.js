ee0f73018050aad6ced76af61359f7d9f2a1ac9c
Esorat <kriptatrade@gmail.com>
2022-05-20 19:36:55 +0700

    commit on 'main'

---
+++ build/precache-manifest.94dccaa2e94201054d27615d55d4fa54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5878e3ff8c98f8efd60580afc85890",
+    "url": "/voice-app/index.html"
+  },
+  {
+    "revision": "cac52e58d81fd2c3b75b",
+    "url": "/voice-app/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "68b34e5946b3bc75c990",
+    "url": "/voice-app/static/js/2.0580cd5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/voice-app/static/js/2.0580cd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac52e58d81fd2c3b75b",
+    "url": "/voice-app/static/js/main.eca6bc16.chunk.js"
+  },
+  {
+    "revision": "0427452aa4ca9e3e7ed9",
+    "url": "/voice-app/static/js/runtime-main.d7ef88d1.js"
+  },
+  {
+    "revision": "44f588104844c2bbe6b044a369713f94",
+    "url": "/voice-app/static/media/logo.44f58810.png"
+  }
+]);
